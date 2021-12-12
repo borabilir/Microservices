7a16d85354455dd8f5d9748eecaef25b7a12b6db
Bora Bilir <bora.bilir@outlook.com>
2021-12-12 03:45:19 +0300

    Add project files.

---
+++ Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CategoryDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
