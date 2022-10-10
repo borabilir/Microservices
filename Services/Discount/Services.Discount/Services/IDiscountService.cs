314dbe4ff34a0114550ec0f41e18ac5e159576da
Bora Bilir <bora.bilir@outlook.com>
2022-10-11 00:34:01 +0300

    Added discount microservice

---
+++ Services/Discount/Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Dtos;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Models.Discount>>> GetAll();
+
+        Task<Response<Models.Discount>> GetById(int id);
+
+        Task<Response<NoContent>> Save(Models.Discount discount);
+
+        Task<Response<NoContent>> Update(Models.Discount discount);
+
+        Task<Response<NoContent>> Delete(int id);
+
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
